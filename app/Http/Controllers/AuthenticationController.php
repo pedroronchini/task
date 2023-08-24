cd7d9a6ab8576360de7622b3f485ea6fb005aa7a
Pedro Ronchini <pe-ronchini@hotmail.com>
2023-08-24 20:55:48 -0300

    create AuthenticationController

---
+++ app/Http/Controllers/AuthenticationController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+
+
+class AuthenticationController extends Controller {
+  public function login(Request $request) 
+  {
+    $credentials = $request->validate([
+      'email' => 'required|email',
+      'password' => 'required'
+    ]);
+
+    if (!Auth::attempt($credentials)) {
+      return response()->json(['Not Authorized'], Response::HTTP_UNAUTHORIZED);
+    } else {
+      return response()->json(Auth::user());
+    }
+  }
+}
