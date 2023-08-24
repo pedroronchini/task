5be49ed42ddc04d21fcdb69668430a1e15e8c728
Pedro Ronchini <pe-ronchini@hotmail.com>
2023-08-24 19:14:02 -0300

    edit TaskModel

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -8,4 +8,30 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     use HasFactory;
+
+    public $timestamps = false;
+
+    protected $table = 'users';
+    protected $fillable = [
+        'id',
+        'title',
+        'description',
+        'attached_files',
+        'completed',
+        'created_at',
+        'completed_at',
+        'updated_at',
+        'deleted_at',
+        'user_id'
+    ];
+
+    protected $casts = [
+        'attached_files' => 'array',
+        'completed' => 'boolean'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
