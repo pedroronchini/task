7f7b31e48cb8fa31ad9f1a2fe39b0f3cce4ffae7
Pedro Ronchini <pe-ronchini@hotmail.com>
2023-08-24 19:30:10 -0300

    edit TaskController

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,9 +2,63 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Task;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
 
 class TaskController extends Controller
 {
-    //
+    public function index()
+    {
+        if (Auth::check()) {
+            $tasks = Task::all();
+            return response()->json($tasks);
+        }
+        
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'title' => 'required|string',
+            'description' => 'nullable|string',
+            'attached_files' => 'nullable|array',
+            'completed' => 'boolean',
+            'user_id' => 'required|exists:users,id'
+        ]);
+
+        $task = Task::create($data);
+
+        return response()->json($task, Response::HTTP_CREATED);
+    }
+
+    public function show($id)
+    {
+        $task = Task::findOrFail($id);
+        return response()->json($task);
+    }
+
+    public function update(Request $request, $id) 
+    {
+        $task = Task::findOrFail($id);
+
+        $data = $request->validate([
+            'title' => 'required|string',
+            'description' => 'nullable|string',
+            'attached_files' => 'nullable|array',
+            'completed' => 'boolean',
+            'user_id' => 'required|exists:users,id'
+        ]);
+
+        $task->update($data);
+        return response()->json($task);
+    }
+
+    public function destroy($id)
+    {
+        $task = Task::findOrFail($id);
+        $task->delete();
+        return response()->json(null, Response::HTTP_NO_CONTENT);
+    }
 }
