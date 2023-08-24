8668a1d30177491051a7d615e5f7e0bf116b711a
Pedro Ronchini <pe-ronchini@hotmail.com>
2023-08-24 19:07:13 -0300

    create table, model and controller tasks

---
+++ database/migrations/2023_08_24_214427_create_tasks_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->json('attached_files')->nullable();
+            $table->boolean('completed')->default(false);
+            $table->dateTime('created_at')->useCurrent();
+            $table->dateTime('completed_at')->nullable();
+            $table->dateTime('updated_at')->useCurrent();
+            $table->dateTime('deleted_at')->nullable();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
