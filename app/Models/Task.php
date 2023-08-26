fea5fd046ff8da98fc4a6ef98906f6f910b95237
Pedro Ronchini <pe-ronchini@hotmail.com>
2023-08-26 17:00:10 -0300

    edit Task model

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -11,7 +11,7 @@ class Task extends Model
 
     public $timestamps = false;
 
-    protected $table = 'users';
+    protected $table = 'tasks';
     protected $fillable = [
         'id',
         'title',
