80b7c915c120a0db6b22af931f7c5ef028ae82d2
Pedro Ronchini <pe-ronchini@hotmail.com>
2023-08-26 19:37:21 -0300

    add bootstrap

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-  <link href="{{ asset('css/bootstrap.css') }}" rel="stylesheet">
+  <!-- <link href="{{ asset('css/bootstrap.css') }}" rel="stylesheet"> -->
   <link rel="stylesheet" href="../css/app.css">
 
   <title>Tasks</title>
